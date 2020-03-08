430304918fe244e1a594b9b6186ec500e76e3164
sebastianbenitez <58483040+sebastianbenitez@users.noreply.github.com>
2020-03-08 17:06:07 -0300

    Agregado API

---
+++ APICRUD/APICRUD/Controllers/CountryController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICRUD.DAL.DAO;
+using APICRUD.DAL.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private CountryDAO countryDAO;
+        public CountryController()
+        {
+            countryDAO = new CountryDAO();
+        }
+
+        
+        [HttpGet]
+        public IEnumerable<CountryDTO> Get()
+        {
+            return countryDAO.GetAll();
+        }
+
+        
+        [HttpGet("{id}")]
+        public CountryDTO Get(int id)
+        {
+            return countryDAO.GetById(id);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] CountryDTO country)
+        {
+            try
+            {
+                var resp = countryDAO.Insert(country);
+                return Ok(resp <= 0 ? "Ningun registro Afectado" : "Insercion exitosa");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error al insertar: {e.Message}");
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] CountryDTO country)
+        {
+            try
+            {
+                var resp = countryDAO.Update(country);
+                return Ok(resp <= 0 ? "Ningun registro Afectado" : "Edicion exitosa");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error al editar: {e.Message}");
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] CountryDTO country)
+        {
+            try
+            {
+                var resp = countryDAO.Delete(country);
+                return Ok(resp <= 0 ? "Ningun registro Afectado" : "Eliminacion exitosa");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error al eliminar: {e.Message}");
+            }
+        }
+    }
+}
---
+++ APICRUD/APICRUD/DAL/DAO/CountryDAO.cs
@@ -0,0 +1,75 @@
+﻿using APICRUD.DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICRUD.DAL.DAO
+{
+    public class CountryDAO : DAOBase
+    {
+        private CountryDTO GetCountryDTO(SqlDataReader reader)
+        {
+            return new CountryDTO
+            {
+                Id = reader[reader.GetOrdinal("id")] as int? ?? default,
+                Name = reader[reader.GetOrdinal("name")] as string
+            };
+        }
+
+        private string selectAllQuery = @"
+            select *
+            from country
+        ";
+
+        public IEnumerable<CountryDTO> GetAll()
+        {
+            var result = new List<CountryDTO>();
+            var reader = GetReader(selectAllQuery);
+            while (reader.Read())
+            {
+                result.Add(GetCountryDTO(reader));
+            }
+            reader.Close();
+            return result;
+        }
+
+        public CountryDTO GetById(int id)
+        {
+            var result = new CountryDTO();
+            var query = @$"{selectAllQuery}
+                            where id = {id}";
+            var reader = GetReader(query);
+            if (reader.Read())
+            {
+                result = GetCountryDTO(reader);
+            }
+            reader.Close();
+            return result;
+        }
+
+        public int Insert(CountryDTO country)
+        {
+            var query = @$"insert into country(id,name)
+                          select max(id)+1,'{country.Name}'
+		                  from country";
+            return GetNonQueryResponse(query);
+        }
+
+        public int Update(CountryDTO country)
+        {
+            var query = @$"update country
+                           set name='{country.Name}'
+		                   where id={country.Id}";
+            return GetNonQueryResponse(query);
+        }
+
+        public int Delete(CountryDTO country)
+        {
+            var query = @$"delete country
+		                   where id={country.Id}";
+            return GetNonQueryResponse(query);
+        }
+    }
+}
